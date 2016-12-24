24f4c3794f30ecf564a8d147a72c433e0823fe03
Fabio Salvalai <fabio@seesharp.ch>
2016-12-24 13:38:45 +0100

    be more verbose

--- NuGet/PublishPackage.cmd
+++ NuGet/PublishPackage.cmd
@@ -1,2 +1,4 @@
+echo "Create Package"
 ..\src\.nuget\nuget pack  ..\src\Autofac.TypedFactories\Autofac.TypedFactories.csproj
+echo "Create Symbol Package"
 ..\src\.nuget\nuget pack  ..\src\Autofac.TypedFactories\Autofac.TypedFactories.csproj -symbols
